2d85b5810b1424cccf3979dea21c21bd2b4bbe0b
Akash_03 <akash.dange0307@gmail.com>
2023-06-08 14:29:22 +0530

    second commit

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ app.post("/", (req, res) => {
     const request = https.request(url, options, (response) => {
 
         if(response.statusCode===200){
-          res.sendFile(__dirname+"dist/success.html");
+          res.sendFile(__dirname+"/success.html");
         }
         else{
-           res.sendFile(__dirname+"dist/failure.html");
+           res.sendFile(__dirname+"/failure.html");
         }
 
         response.on("data", (data) => {
